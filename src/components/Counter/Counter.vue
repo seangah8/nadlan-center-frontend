c8cc1e591b6c29198c4837f412cb36a93467d31a
seangah8 <seangah8@gmail.com>
2025-07-30 03:08:40 +0300

    Nadlan Center startup

---
+++ src/components/Counter/Counter.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+    <p class="line">You clicked {{ count }} times</p>
+    <button @click="increment">Click me</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const count = ref<number>(0)
+
+  function increment() : void {
+    count.value++
+  }
+</script>
+
+<style scoped lang="scss" src="./Counter.scss"></style>
+
